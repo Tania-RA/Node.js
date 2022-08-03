c16451ce888d289986cad90181a1a4e6e26c0a03
Tania-RA <taniariazpk786@gmail.com>
2022-08-03 12:34:25 +0200

    Completed all assignments and passed all tests.

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -6,10 +6,38 @@ const http = require('http');
  * will fail.
  */
 function createServer(port) {
+  // eslint-disable-next-line no-unused-vars
   let state = 10;
+  let currentState = state;
 
-  const server = http.createServer((request, response) => {
+  const server = http.createServer((req, res) => {
     // TODO: Write your homework code here
+    console.log(req.method, req.url);
+    switch (req.url) {
+      case '/': // Default endpoint
+        break;
+      case '/state':
+        break;
+      case '/add':
+        state++;
+        break;
+      case '/subtract':
+        state--;
+        break;
+      case '/reset':
+        state = 10;
+        break;
+      default:
+        res.statusCode = 404;
+        let error = 'Not found';
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ error }, null, 2));
+        return;
+    }
+    currentState = state;
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ 'state': currentState }, null, 2));
   });
 
   return server;
